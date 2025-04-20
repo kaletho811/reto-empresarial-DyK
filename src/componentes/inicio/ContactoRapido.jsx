71d83789d25db18c5ec7f6bfe599785ca24cbe1b
Kaleth Barrios <kaletht@yahoo.com>
2025-04-20 17:40:49 -0500

    actualizacion contactos

--- src/componentes/inicio/ContactoRapido.jsx
+++ src/componentes/inicio/ContactoRapido.jsx
@@ -4,9 +4,15 @@ const ContactoRapido = () => {
     return (
         <section className="contacto-rapido">
         <h2>Contáctanos Rápidamente</h2>
-        <p>WhatsApp: <a href="https://wa.me/573001234567">+57 300 123 4567</a></p>
-        <p>Correo: <a href="mailto:info@coonadoc.co">info@coonadoc.co</a></p>
-        <p>Redes sociales: @Coonadoc</p>
+        <p>WhatsApp: <a href="https://api.whatsapp.com/send?phone=573115019158">+57 311 501 9158</a></p>
+        <p>Linea fija: +57 601 7523313</p>
+        <p>Correo contacto 1: gerencia@coonadoc.co</p>
+        <p>Correo correo contacto 2: servicioalclientecoonadoc@gmail.com</p>
+        <p>REDES SOCIALES</p>
+        <p>FACEBOOK: <a href="https://www.facebook.com/COONADOC/">Click aqui</a></p>
+        <p>YOUTUBE: <a href="https://www.youtube.com/channel/UCV9denEEUEbxAt2uLwbnaDg">Click aqui</a></p>
+        <p>X: <a href="https://x.com/Coonadoc">Click aqui</a></p>
+        <p>INSTAGRAM: <a href="https://www.instagram.com/coopcoonadoc/">Click aqui</a></p>
         </section>
     );
 };
