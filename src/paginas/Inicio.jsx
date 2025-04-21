2335c6c45363cc43b607e85a58daed6f718c70e4
Kaleth Barrios <kaletht@yahoo.com>
2025-04-20 19:12:24 -0500

    actualizacion estilos 2

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import NoticiasDestacadas from "../componentes/inicio/NoticiasDestacadas";
 import ContactoRapido from "../componentes/inicio/ContactoRapido";
+import Navegacion from "../componentes/Navegacion";
 
 const Inicio = () => {
   return (
-    <div>
-      <img src="../imagenes/logo.png" alt="logo COONADOC" className="logo"/>
+    <><div className="parte-superior">
+      <img src="../imagenes/logo.png" alt="logo COONADOC" className="logo" />
       <h1>Bienvenido a COONADOC</h1>
+      <Navegacion />
+    </div>
+    <div>
       <NoticiasDestacadas />
       <ContactoRapido />
-    </div>
+    </div></>
   );
 };
 
